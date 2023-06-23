3c81e797901e66e10d235c5df012582778e0b3ae
Beril (Beth) <90333992+bberildundar@users.noreply.github.com>
2023-06-23 16:04:31 +0200

    updated SQL file

--- sql/petwise-wd2.sql
+++ sql/petwise-wd2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Generation Time: Jun 19, 2023 at 05:27 PM
+-- Generation Time: Jun 23, 2023 at 01:58 PM
 -- Server version: 10.11.2-MariaDB-1:10.11.2+maria~ubu2204
 -- PHP Version: 8.1.17
 
@@ -31,8 +31,17 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `email` varchar(1000) NOT NULL,
   `password` varchar(1000) NOT NULL,
-  `role` tinyint(1) NOT NULL
+  `role` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`, `role`) VALUES
+(5, 'mark@email.com', '$2y$10$/XSKRG95BmTGTwpPURrkIeM5d.X.L1tJe54Sr452DVz8.1hA.Ho/O', 1),
+(7, 'beth@email.com', '$2y$10$QTPfBUhrAqPGuqci4pSP9.JGf/lrY1Wc./..GT5WmWAa2MvyKRyUq', 0),
+(25, 'user@email.com', '$2y$10$zqRfZ5g1y8iY5KFl6pYeIuNX5rAsNsSWKmm/4fMEU3H.NpAhdLRxO', 0);
 
 -- --------------------------------------------------------
 
@@ -82,13 +91,13 @@ ALTER TABLE `vet`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `vet`
 --
 ALTER TABLE `vet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
